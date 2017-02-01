0e212baa55bf2aa67e31fbbb74b0cd0daa67ec76
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-01 14:47:36 -0600

    Exercise 7.6
    updated to create users table
    added foreign key uer_id to ads table

--- resources/migration.sql
+++ resources/migration.sql
@@ -1,10 +1,24 @@
-CREATE DATABASE if NOT EXISTS adlister_db;
+CREATE DATABASE IF NOT EXISTS  adlister_db;
 
 USE adlister_db;
 
-CREATE TABLE if NOT EXISTS ads(
-  id int UNSIGNED not NULL AUTO_INCREMENT,
-  title VARCHAR(400) not NULL,
-  description TEXT,
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(240) NOT NULL,
+  email VARCHAR(240) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+  id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id     INT UNSIGNED NOT NULL,
+  title       VARCHAR(240) NOT NULL,
+  description TEXT         NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
 );
