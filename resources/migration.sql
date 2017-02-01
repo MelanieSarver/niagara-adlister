adfff4b9c40bae37533f6fe8f41fbb1d0f97727b
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-01 12:10:09 -0600

    Exercise 7.6
    Created adlister_db

---
+++ resources/migration.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE if NOT EXISTS adlister_db;
+
+USE adlister_db;
+
+CREATE TABLE if NOT EXISTS ads(
+  id int UNSIGNED not NULL AUTO_INCREMENT,
+  title VARCHAR(400) not NULL,
+  description TEXT,
+  PRIMARY KEY (id)
+);
