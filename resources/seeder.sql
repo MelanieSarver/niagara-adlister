dd372d661f04272fdf1a5c654be37335891ec01c
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-01 12:10:21 -0600

    Exercise 7.6
    Created a seeder for adlister_db

---
+++ resources/seeder.sql
@@ -0,0 +1,8 @@
+USE adlister_db;
+
+INSERT into ads(title, description) VALUES
+  ('playstation for sale', 'This is a slightly used playstation'),
+  ('Super Nintendo', 'Get your game on with this old-school classic!'),
+  ('Junior Java Developer Position', 'Minimum 7 years of experience required. You will
+  be working in the scripting language for Java, JavaScript'),
+  ('javaScript Developer Needed', 'Must have strong Java skills');
